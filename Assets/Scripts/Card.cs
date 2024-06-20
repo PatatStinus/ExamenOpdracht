4de5ce04f9a74fccb7d5591bb4095b3c49ab7774
PatatStinus <stijnrpd@gmail.com>
2024-06-20 15:20:02 +0200

    TMPRO Packages + Card game names

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,9 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Threading;
+using TMPro;
 
 public class Card : MonoBehaviour
 {
     public enum Cards { MudRunner, CatchAndRun, AnimalBoxing}
     public Cards CardType = Cards.MudRunner;
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(0.2f);
+
+        string gameName = "";
+
+        if (CardType == Cards.MudRunner) gameName = "Mud Bath";
+        else if (CardType == Cards.CatchAndRun) gameName = "Catch And Run";
+        else if (CardType == Cards.AnimalBoxing) gameName = "Animal Boxing";
+
+        transform.GetChild(0).GetComponent<TextMeshPro>().text = gameName;
+    }
 }
